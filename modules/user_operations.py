32fced803bed8a84578b855adb528e438a6e0c93
ManishaHub-Dev <manishab1315@gmail.com>
2024-07-31 07:08:04 -0500

    Added utility function to get all users

--- modules/user_operations.py
+++ modules/user_operations.py
@@ -7,3 +7,10 @@ def add_user(name, age, email):
         c.execute("INSERT INTO users (name, age, email) VALUES (?, ?, ?)", (name, age, email))
         logging.info(f"Inserted user {name}")
         conn.commit()
+        
+# Utility function to get all users from the database
+def get_all_users():
+    with get_connection() as conn:
+        c = conn.cursor()
+        c.execute("SELECT * FROM users")
+        return c.fetchall()
