8d2cd2dab692412a4b9773bba8a77998535835a8
ManishaHub-Dev <manishab1315@gmail.com>
2024-08-14 00:00:09 -0500

    Added unit tests for employee management functions

---
+++ EmployeeManagementSystem/test_employee_management.py
@@ -0,0 +1,14 @@
+import unittest
+from main import add_employee, remove_employee
+
+class TestEmployeeManagement(unittest.TestCase):
+    def test_add_employee(self):
+        # Setup and test the add_employee function
+        pass
+
+    def test_remove_employee(self):
+        # Setup and test the remove_employee function
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
