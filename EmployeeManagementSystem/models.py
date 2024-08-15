6d16e95764633ca14a396b8440372c082edb5558
ManishaHub-Dev <manishab1315@gmail.com>
2024-08-15 11:27:23 -0500

    Created Incentive model class

--- EmployeeManagementSystem/models.py
+++ EmployeeManagementSystem/models.py
@@ -3,3 +3,10 @@ class Department:
         self.id = id
         self.name = name
         self.manager_id = manager_id
+        
+class Incentive:
+    def __init__(self, id, employee_id, incentive_amount, date_awarded):
+        self.id = id
+        self.employee_id = employee_id
+        self.incentive_amount = incentive_amount
+        self.date_awarded = date_awarded
